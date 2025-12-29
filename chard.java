fa7d624a46e4d0bd77fa5c2f4e7a47f205d15aba
rbharinishree <rbharinishree@gmail.com>
2025-12-29 12:20:10 +0530

    Add files via upload

---
+++ chard.java
@@ -0,0 +1,7 @@
+public class chard {
+       public static void main(String[] args) {
+             char fish= 'F';
+             char ful='@';
+             System.out.println("The gold" + fish + ful + "gmail.com");
+}
+}
