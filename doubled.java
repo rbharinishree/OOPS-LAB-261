c74f175b669ff37bbb225b748bb77197416ccb1c
rbharinishree <rbharinishree@gmail.com>
2026-01-16 00:11:29 +0530

    Add files via upload

---
+++ doubled.java
@@ -0,0 +1,13 @@
+public class doubled{
+    public static void main(String[] args) {
+        double a = 10.5;
+        double b = 40.3;
+
+        double sum = a + b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("Sum = " + sum);
+
+    }
+}
